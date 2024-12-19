383c8117422dcc9cb6bcc6185862f56730cc4aaa
maziyyahjaf <mazi3ah@gmail.com>
2024-12-19 19:59:15 +0800

    worked on telegram notification service

--- src/main/java/com/example/maziyyah/mood_tracker/service/TelegramNotificationService.java
+++ src/main/java/com/example/maziyyah/mood_tracker/service/TelegramNotificationService.java
@@ -1,15 +1,22 @@
 package com.example.maziyyah.mood_tracker.service;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.maziyyah.mood_tracker.repository.TelegramNotificationRepo;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
 
 @Service
 public class TelegramNotificationService {
@@ -33,10 +40,45 @@ public class TelegramNotificationService {
     // need the chatid of user
     // need the chatid of lovedones
 
+    public void sendMessage(String userId) {
+
+        Optional<String> userChatIdOpt = getUserChatId(userId);
+        if (userChatIdOpt.isEmpty()) {
+            logger.warn("Cannot send message because user {} has no Telegram chat ID linked.", userId);
+            return;
+        }
+
+        String userChatId = userChatIdOpt.get();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/json");
+
+        String message = "Things will be okay";
+
+        JsonObject jsonObject = Json.createObjectBuilder()
+                                .add("chat_id", userChatId)
+                                .add("text", message)
+                                .build();
+
+        RequestEntity<String> req = RequestEntity
+                                    .post(URI.create(buildSendMessageUrl()))
+                                    .headers(headers)
+                                    .body(jsonObject.toString());
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(req, String.class);
+            logger.info("Response: Status={}, Body={}", response.getStatusCode(), response.getBody());
+            System.out.println(response);
+        } catch (Exception e)    {
+            logger.error("Failed to send Telegram message to userId: {}. Error: {}", userId, e.getMessage(), e);
+
+        }
+
+    }
+
 
     public String buildSendMessageUrl() {
         return UriComponentsBuilder.fromUriString(telegramBotMessageUrl)
-                                    .pathSegment("bot" + telegramBotToken, "sendMessage")
+                                    .pathSegment("bot" + telegramBotToken)
+                                    .pathSegment("sendMessage")
                                     .toUriString();
     }
     
